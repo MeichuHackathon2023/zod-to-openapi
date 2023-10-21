2a0006c9f8a07a04ba87e7fa7a676a8010ec5ab3
Ian Wang <b09902007@csie.ntu.edu.tw>
2023-10-21 16:32:51 +0800

    fix: ZodLazy type syntax error

--- src/lib/zod-is-type.ts
+++ src/lib/zod-is-type.ts
@@ -29,7 +29,7 @@ type ZodTypes = {
   ZodUnion: z.ZodUnion<any>;
   ZodDiscriminatedUnion: z.ZodDiscriminatedUnion<any, any>;
   ZodUnknown: z.ZodUnknown;
-  ZodLazy: z.ZodLazy;
+  ZodLazy: z.ZodLazy<any>;
   ZodVoid: z.ZodVoid;
   ZodDate: z.ZodDate;
 };
